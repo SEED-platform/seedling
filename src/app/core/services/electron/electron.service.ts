e683ad15bcb7e8f35fe9a986712a85b77687ebfc
Alex Swindler <Alex.Swindler@nrel.gov>
2020-09-15 14:55:17 -0600

    Fixed production reload issue, added sequelize and pg

--- src/app/core/services/electron/electron.service.ts
+++ src/app/core/services/electron/electron.service.ts
@@ -6,6 +6,8 @@ import { ipcRenderer, webFrame, remote } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as pg from 'pg';
+import { Sequelize } from 'Sequelize';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,8 @@ export class ElectronService {
   childProcess: typeof childProcess;
   fs: typeof fs;
   path: typeof path;
+  pg: typeof pg;
+  Sequelize: typeof Sequelize;
 
   get isElectron(): boolean {
     return !!(window && window.process && window.process.type);
@@ -28,12 +32,14 @@ export class ElectronService {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.webFrame = window.require('electron').webFrame;
 
-      // If you wan to use remote object, pleanse set enableRemoteModule to true in main.ts
+      // If you want to use remote object please set enableRemoteModule to true in main.ts
       this.remote = window.require('electron').remote;
 
       this.childProcess = window.require('child_process');
       this.fs = window.require('fs');
       this.path = window.require('path');
+      this.pg = this.remote.require('pg');
+      this.Sequelize = this.remote.require('sequelize').Sequelize;
     }
   }
 }
